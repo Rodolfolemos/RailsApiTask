d4555b503a93ddce7d3a2d01bc5a8cf506af28b9
Rodolfo Cesar <roodlemos@gmail.com>
2018-05-26 18:51:03 +0000

    Addins association with tasks for user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
     #validates_presence_of :name
     validates_uniqueness_of :auth_token
     before_create :generate_authentication_token!
-    
+
+    has_many :tasks, dependent: :destroy
     def info
       "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
       
