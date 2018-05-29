50a6a0b00aef34a7842f3e376542f7a888f07d51
Rodolfo Cesar <roodlemos@gmail.com>
2018-05-29 18:56:22 +0000

    installing devise_token_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules.
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  :recoverable, :rememberable, :trackable, :validatable
+  
+  include DeviseTokenAuth::Concerns::User
     #attr_accessor :name
     #validates_presence_of :name
     validates_uniqueness_of :auth_token
