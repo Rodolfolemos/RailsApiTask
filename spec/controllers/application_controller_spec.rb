de7593fb1250097c6c219808a9e99db3ff9b4e1f
Rodolfo Cesar <roodlemos@gmail.com>
2018-05-29 19:17:28 +0000

    Adjusting controllers

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  describe 'include the corret concerns' do
-    it { expect(controller.class.ancestors).to include(Authenticable)  } 
-  end
-end
